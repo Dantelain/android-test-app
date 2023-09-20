81ed9e9a0d3678a59b73e15b8a07f4496097a993
Dantelain <myamskov@yandex.ru>
2023-09-20 22:29:50 +0400

    исправлен build.bat

--- sign.bat
+++
@@ -1,4 +0,0 @@
-
-set APKSIGNER="C:\Users\jms\AppData\Local\Android\Sdk\build-tools\34.0.0\apksigner.bat"
-
-%APKSIGNER% sign --ks my-release-key.jks --ks-pass "pass:123456" --out bin\out-release.apk bin\out.unsigned.aligned.apk
